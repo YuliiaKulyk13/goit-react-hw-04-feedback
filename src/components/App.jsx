391b8d6833c6198219230dc13fa04a1b9f9b4289
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-04-03 14:24:47 -0400

    adding hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
+
 import { FeedbackOptions } from './Feedback/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 import { Layout } from './Layout /Layout.styled';
 
-export class App extends Component {
-  state = {
-    Good: 0,
-    Neutral: 0,
-    Bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    return this.state.Good + this.state.Neutral + this.state.Bad;
-  };
-  countPositiveFeedbackPercentage = () => {
-    return (
-      Math.round(
-        (this.state.Good * 100) /
-          (this.state.Good + this.state.Neutral + this.state.Bad)
-      ) || 0
-    );
-  };
+  const countTotalFeedback = good + neutral + bad;
 
-  onClickBtn = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
+  const countPositiveFeedbackPercentage =
+    Math.round((good * 100) / (good + neutral + bad)) || 0;
 
-  render() {
-    return (
-      <Layout>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onClickBtn}
+  const onClickBtn = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        return;
+    }
+  };
+  return (
+    <Layout>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onClickBtn}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        {countTotalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
-        </Section>
-        <Section title={'Statistics'}>
-          {this.countTotalFeedback() ? (
-            <Statistics
-              good={this.state.Good}
-              neutral={this.state.Neutral}
-              bad={this.state.Bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message={'There is no feedback'} />
-          )}
-        </Section>
-      </Layout>
-    );
-  }
-}
+        ) : (
+          <Notification message={'There is no feedback'} />
+        )}
+      </Section>
+    </Layout>
+  );
+};
